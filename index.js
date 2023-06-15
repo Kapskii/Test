6aa8e8374b28ac91e453cba8064ebb0c7dc3cd90
Kapskii <kapskij20@gmail.com>
2023-06-15 16:10:00 +0300

    validatePin

--- index.js
+++ index.js
@@ -131,3 +131,22 @@ function problem(x) {
      
      return true;
    }
+
+
+
+   function validatePIN(pin) {
+    const length = pin.length; 
+    
+    
+    if (length !== 4 && length !== 6) {
+      return false
+    }
+
+  for (let i = 0; i < pin.length; i++) {
+    if (pin[i] < '0' || pin[i] > '9') {
+    
+        return false;
+      }  
+    } 
+    return true;
+  }
