f69fbd439160e98e4df50a5b76f7476157209346
Kapskii <kapskij20@gmail.com>
2023-06-15 14:16:09 +0300

    pangrams

--- index.js
+++ index.js
@@ -117,3 +117,17 @@ function problem(x) {
      return "Error"
    }
   }
+
+
+  function isPangram(string){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+     let regex = /\string/g;
+     let lowercase = string.toLowerCase().replace(regex, "");
+       for(let i = 0; i < alphabet.length; i++){
+       if(lowercase.indexOf(alphabet[i]) === -1){
+         return false;
+       }
+      }
+     
+     return true;
+   }
