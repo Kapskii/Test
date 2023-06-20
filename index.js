23a6fd2d50e2ef7a38e7deb2cd04db87b886dba4
Kapskii <kapskij20@gmail.com>
2023-06-20 15:56:52 +0300

    Get the Middle Character

--- index.js
+++ index.js
@@ -289,3 +289,13 @@ function countdown(n) {
       return numbers;
     }
   };
+
+
+  function getMiddle(s)
+{
+let evenOdd = s.length % 2 === 0
+  let minMid = s.charAt(s.length/2-1)
+  let mid = s.charAt(s.length/2)
+  
+  return (evenOdd == true)? minMid.concat(mid): mid
+}
