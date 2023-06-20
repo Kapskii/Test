d1a9ab19f88bcf4f2411cf0ac8d34029fda6e9fa
Kapskii <kapskij20@gmail.com>
2023-06-20 12:14:36 +0300

    classPoints

--- index.js
+++ index.js
@@ -177,3 +177,13 @@ do {
   i++;
 } while (i < 5)
 console.log(myArray)
+
+
+function betterThanAverage(classPoints, yourPoints) {
+  var classAvg = 0;
+    for (var i = 0; i < classPoints.length; i++){
+      classAvg += classPoints[i]; 
+    }
+    classAvg = classAvg/classPoints.length; 
+    return yourPoints > classAvg;
+  }
