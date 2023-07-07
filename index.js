28c91f124753f285e57dc338619c26128902b082
Kapskii <kapskij20@gmail.com>
2023-07-07 15:46:22 +0300

    SpreadOperator

--- index.js
+++ index.js
@@ -576,3 +576,11 @@ function findAverage(array) {
           }
           return total;
         }
+
+
+        const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+        let arr2;
+
+        arr2 = [...arr1];
+
+        console.log(arr2);
