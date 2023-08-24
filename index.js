e353b6584111573c0978ee38bae976014f6c3634
Kapskii <kapskij20@gmail.com>
2023-08-24 14:36:41 +0300

    Slice and Splice

--- index.js
+++ index.js
@@ -816,3 +816,16 @@ function truncateString(str, num) {
   }
   
   titleCase("I'm a little tea pot");
+
+  //Slice and Splice
+
+  function frankenSplice(arr1, arr2, n) {
+    let newArr2 = arr2.slice()
+    for (let i = 0; i < arr1.length; i++){
+      newArr2.splice(n, 0, arr1[i])
+      n++
+    }
+    return newArr2;
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
