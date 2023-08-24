845b29608e9416c2999bbb58f2b7e8f8fd94f2ce
Kapskii <kapskij20@gmail.com>
2023-08-24 12:54:22 +0300

    Truncate a String

--- index.js
+++ index.js
@@ -774,3 +774,14 @@ function confirmEnding(str, target) {
 }
 
 confirmEnding("Bastian", "n");
+
+//Truncate a String
+
+function truncateString(str, num) {
+  if (str.length > num) {
+   return str.slice(0, num) + '...';
+  }
+  return str
+  }
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
