b6032a254b7715b5749eb221bf0bea95ab5ab7be
Kapskii <kapskij20@gmail.com>
2023-08-24 12:20:10 +0300

    Return Largest Numbers in Arrays

--- index.js
+++ index.js
@@ -748,3 +748,21 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//Return Largest Numbers in Arrays
+
+function largestOfFour(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++){
+    let largNum = arr[i][0];
+    for (let j = 0; j < arr.length; j++){
+      if (arr[i][j] > largNum) {
+        largNum = arr[i][j];
+      }
+    }
+    result[i] = largNum
+  }
+  return result
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
